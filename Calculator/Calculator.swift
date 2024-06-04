8f5dc997d3f0cfbc8f1a64b49d977c176042e0dd
sh990920 <sh990920@gmail.com>
2024-06-04 15:56:47 +0900

    Fix - 기능 구현 수정 및 유의사항 준수

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -11,28 +11,37 @@ class Calculator {
     // Lv1
     // 더하기, 빼기, 나누기, 곱하기 연산을 수행할 수 있는 Calculator 클래스 만들기
     
+    // fix - 연산 후 출력으로 변경
+    private let num1: Int
+    private let num2: Int
+    
+    init(_ num1: Int, _ num2: Int) {
+        self.num1 = num1
+        self.num2 = num2
+    }
+    
     // 더하기
-    func plus(_ num1: Int, _ num2: Int) -> Int {
-        return num1 + num2
+    func plus() {
+        print("덧셈 결과 : \(self.num1 + self.num2)")
     }
     // 빼기
-    func minus(_ num1: Int, _ num2: Int) -> Int {
-        return num1 - num2
-    }
-    // 나누기
-    func division(_ num1: Int, _ num2: Int) -> Int {
-        return num1 / num2
+    func minus() {
+        print("뺄셈 결과 : \(self.num1 - self.num2)")
     }
     // 곱하기
-    func multiplication(_ num1: Int, _ num2: Int) -> Int {
-        return num1 * num2
+    func multiplication() {
+        print("곱셈 결과 : \(self.num1 * self.num2)")
+    }
+    // 나누기
+    func division() {
+        print("나눗셈 결과 : \(self.num1 / self.num2)")
     }
     
     // Lv2
     // Lv1에서 만든 Calculator 클래스에 “나머지 연산”이 가능하도록 코드를 추가하고, 연산 진행 후 출력
     
     // 나머지 연산
-    func remainder(_ num1: Int, _ num2: Int) -> Int {
-        return num1 % num2
+    func remainder() {
+        print("num1을 num2 로 나눈 나머지 : \(self.num1 % self.num2)")
     }
 }
--- Calculator/main.swift
+++ Calculator/main.swift
@@ -8,21 +8,38 @@
 import Foundation
 
 // 생성한 클래스를 이용하여 연산을 진행하고 출력
-let calculator = Calculator()
+//let calculator = Calculator()
+//
+//// 숫자1
+//var num1 = 10
+//// 숫자2
+//var num2 = 3
+//
+//// 더하기
+//print(calculator.plus(num1, num2))
+//// 뺴기
+//print(calculator.minus(num1, num2))
+//// 나누기
+//print(calculator.division(num1, num2))
+//// 곱하기
+//print(calculator.multiplication(num1, num2))
+//
+//// Lv2 나머지 연산 출력
+//print(calculator.remainder(num1, num2))
 
-// 숫자1
-var num1 = 10
-// 숫자2
-var num2 = 3
+// 유의사항
+// 사용자의 입력을 직접 받아서 입력한다.
+print("첫번째 숫자를 입력하세요 : ", terminator: "")
+let num1 = readLine()
+print("두번째 숫자를 입력하세요 : ", terminator: "")
+let num2 = readLine()
 
-// 더하기
-print(calculator.plus(num1, num2))
-// 뺴기
-print(calculator.minus(num1, num2))
-// 나누기
-print(calculator.division(num1, num2))
-// 곱하기
-print(calculator.multiplication(num1, num2))
-
-// Lv2 나머지 연산 출력
-print(calculator.remainder(num1, num2))
+if let num1 = Int(num1!), let num2 = Int(num2!) {
+    let calculator = Calculator(num1, num2)
+    
+    calculator.plus()
+    calculator.minus()
+    calculator.multiplication()
+    calculator.division()
+    calculator.remainder()
+}
